5e5201c9315874374a71256ce5bedf83bb12b1bc
Jakub VIlcek <jakub.vilcek9@gmail.com>
2021-04-12 12:09:42 +0200

    edit add example

--- gympel/example_handler.py
+++ gympel/example_handler.py
@@ -4,21 +4,40 @@ import json
 
 def get_content():
   dirname = os.path.dirname(__file__)
-  print(dirname)
   with open(dirname + "\public\examples.json") as f:
     dzejson = json.load(f)
   example_names = []
   for keys in dzejson:
     example_names.append(keys)
-  print(example_names)
   example_name = random.choice(example_names)
   example = dzejson[example_name]
-  print(example)
   return example
 
 def print_example_by_name(nazov):
   dirname = os.path.dirname(__file__)
-  print(dirname)
   with open(dirname + "\public\examples.json") as f:
     dzejson = json.load(f)
   print(dzejson[nazov])
+
+def add_example(example, nazov):
+    dirname = os.path.dirname(__file__)    
+    with open(dirname + "\public\examples.json") as f:
+        examples = json.load(f)
+    examples[nazov] = example[nazov]
+    print(examples)
+    
+    with open(dirname + "\public\examples.json", 'w') as f:
+        json.dump(examples, f)
+    
+nazov = ""
+example = {nazov:'''VES v1.0 600 400
+CLEAR #0000
+'''}
+
+def remove_example_by_name(nazov):
+  dirname = os.path.dirname(__file__)
+  with open(dirname + "\public\examples.json") as f:
+    dzejson = json.load(f)
+  dzejson.pop(nazov)
+  with open(dirname + "\public\examples.json", 'w') as f:
+    json.dump(dzejson, f)
