2a8fdc7a21b9ebde78fa1fbaa96f58d9a7d02316
Adam Džadoň <adam.dzadon.gmail.com>
2021-10-28 11:13:41 +0200

    Update git href

--- main.py
+++ main.py
@@ -58,4 +58,5 @@ def render():
 
   return serve_pil_image(render.picture) # return converted PIL image object to .png file
 
-app.run()
+if __name__ == "__main__":
+  app.run(host='127.0.0.1', port=80)
